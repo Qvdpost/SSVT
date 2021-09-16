6db8a4aef76b22c660c0fac75fa2599a3b57d4e4
Quinten <qvdpost@gmail.com>
2021-09-16 12:48:37 +0200

    itegrally important

--- Quinten/Wk2/Exercise1.hs
+++ Quinten/Wk2/Exercise1.hs
@@ -24,11 +24,10 @@ tupleToList :: (Integer, Integer, Integer, Integer) -> [Integer]
 tupleToList (a,b,c,d) = [a,b,c,d]
 
 prop_Distribution :: (Integer, Integer, Integer, Integer) -> Integer -> Bool
-prop_Distribution buckets n = all (\x -> abs (x - fraction) < error) (tupleToList buckets)
+prop_Distribution buckets n = all (\x -> fromIntegral (abs (x - fraction)) < error) (tupleToList buckets)
   where
-    fraction = (fromIntegral n :: Float) / 4.0
-    error = 0.05 * fraction
-
+    fraction = n `div` 4
+    error = 0.05 * fromIntegral fraction
 
 exercise1 :: IO ()
 exercise1 = do
