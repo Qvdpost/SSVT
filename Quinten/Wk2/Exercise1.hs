451bedcc5d83e80494e3021fac5f6e386737a7df
Quinten <qvdpost@gmail.com>
2021-09-16 12:39:09 +0200

    floats and ints

--- Quinten/Wk2/Exercise1.hs
+++ Quinten/Wk2/Exercise1.hs
@@ -3,6 +3,7 @@ module Lab2 where
 import Data.Char
 import Helper (exercise)
 import System.Random
+import Data.Foldable
 
 -- Time spent, 30 minutes + ~8 cleanup
 probs :: Int -> IO [Float]
@@ -13,17 +14,27 @@ probs n = do
   return (p : ps)
 
 bucketize :: [Float] -> (Integer, Integer, Integer, Integer) -> (Integer, Integer, Integer, Integer)
-bucketize (x:xs) (a,b,c,d) | x <= 0.25 = bucketize xs ((a+1),b,c,d)
-                           | x <= 0.50 = bucketize xs (a,(b+1),c,d)
-                           | x <= 0.75 = bucketize xs (a,b,(c+1),d)
-                           | otherwise = bucketize xs (a,b,c,(d+1))
+bucketize (x:xs) (a,b,c,d) | x <= 0.25 = bucketize xs (a+1,b,c,d)
+                           | x <= 0.50 = bucketize xs (a,b+1,c,d)
+                           | x <= 0.75 = bucketize xs (a,b,c+1,d)
+                           | otherwise = bucketize xs (a,b,c,d+1)
 bucketize [] buckets = buckets
+
+tupleToList :: (Integer, Integer, Integer, Integer) -> [Integer]
+tupleToList (a,b,c,d) = [a,b,c,d]
+
+prop_Distribution :: (Integer, Integer, Integer, Integer) -> Integer -> Bool
+prop_Distribution buckets n = all (\x -> abs (x - fraction) < error) (tupleToList buckets)
+  where
+    fraction = (fromIntegral n :: Float) / 4.0
+    error = 0.05 * fraction
+
 
 exercise1 :: IO ()
 exercise1 = do
   -- putStrLn $ exercise 1 "XXX"
-    nums <- probs 1000000
-    print (bucketize nums (0,0,0,0))
+    nums <- probs 10000
+    print (prop_Distribution (bucketize nums (0,0,0,0)) 10000)
 
 _main :: IO ()
 _main = do
