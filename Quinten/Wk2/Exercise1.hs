29423093c313eb4959b2862a1e56f3a30b2ad0cc
Quinten <qvdpost@gmail.com>
2021-09-16 12:00:01 +0200

    tuples

--- Quinten/Wk2/Exercise1.hs
+++ Quinten/Wk2/Exercise1.hs
@@ -12,20 +12,18 @@ probs n = do
   ps <- probs (n -1)
   return (p : ps)
 
-bucketize :: [Float] -> [Integer] -> [Integer]
-bucketize (x:xs) [a:b:c:d] | x <= 0.25 = bucketize xs ((a+1):b:c:d)
-                           | x <= 0.50 = bucketize xs (a:(b+1):c:d)
-                           | x <= 0.75 = bucketize xs (a:b:(c+1):d)
-                           | otherwise = bucketize xs (a:b:c:(d+1))
-bucketize (x:xs) _ = error "Please provide 4 buckets"
+bucketize :: [Float] -> (Integer, Integer, Integer, Integer) -> (Integer, Integer, Integer, Integer)
+bucketize (x:xs) (a,b,c,d) | x <= 0.25 = bucketize xs ((a+1),b,c,d)
+                           | x <= 0.50 = bucketize xs (a,(b+1),c,d)
+                           | x <= 0.75 = bucketize xs (a,b,(c+1),d)
+                           | otherwise = bucketize xs (a,b,c,(d+1))
 bucketize [] buckets = buckets
-
 
 exercise1 :: IO ()
 exercise1 = do
   -- putStrLn $ exercise 1 "XXX"
     nums <- probs 1000000
-    print (bucketize nums [0,0,0,0])
+    print (bucketize nums (0,0,0,0))
 
 _main :: IO ()
 _main = do
