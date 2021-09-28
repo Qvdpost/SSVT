14122197b054fa7c71dc45272c7d3282556aafe0
Lily O'Sullivan <lilyosullivan@pm.me>
2021-09-28 21:47:58 +0200

    Progress Update

--- Quinten/Wk3/Exercise1.hs
+++ Quinten/Wk3/Exercise1.hs
@@ -10,20 +10,28 @@ module Exercise1 where
 
 import Data.List
 import Helper (exercise)
-import Lecture3
+import Lecture3 (allVals,evl,Form(Impl,Equiv))
 import System.Random
 import Test.QuickCheck
 
-
+-- By definition a contradiction must evaluate all values to False
 contradiction :: Form -> Bool
+contradiction f = and [not (evl x f)  | x <- allVals f]
 
+-- Like above, except must evaluate to True
 tautology :: Form -> Bool
+tautology f = and [  evl x f  | x <- allVals f]
 
--- | logical entailment 
+-- | logical entailment
+-- I haven't heard of this before, following this
+-- https://www.quora.com/What-is-tautological-entailment
 entails :: Form -> Form -> Bool
+entails f ff = tautology (Impl f ff)
 
 -- | logical equivalence
+-- Assuming by name, this is from the above
 equiv :: Form -> Form -> Bool
+equiv f ff = tautology (Equiv f ff)
 
 {-
   Procedure:
@@ -49,7 +57,6 @@ equiv :: Form -> Form -> Bool
 exercise1 :: IO ()
 exercise1 = do
   putStrLn $ exercise 1 "Definitions of: contradiction,tautology,entails,equiv"
-  putStrLn $ "Example output: "
 
 _main :: IO ()
 _main = exercise1
--- Quinten/Wk3/Exercise4.hs
+++ Quinten/Wk3/Exercise4.hs
@@ -10,9 +10,37 @@ module Exercise4 where
 
 import Data.List
 import Helper (exercise)
-import Lecture3
+import Lecture3 ( Form(..))
 import System.Random
 import Test.QuickCheck
+
+randomProp::IO Form
+randomProp = do
+  prop_num <- randomRIO (1, 3)
+  return (Prop prop_num)
+
+getManyRandomProps::Int -> [IO Form]
+getManyRandomProps n
+  | n > 0 = randomProp : getManyRandomProps (n-1)
+  | n == 0 = []
+  | otherwise = error "Not happy 😥"
+
+randomNegate::IO Form
+randomNegate = do
+  Neg <$> randomProp
+
+randomConjunction::IO Form
+randomConjunction = do
+  p <-randomProp
+  q <-randomProp
+  return (Cnj (p:[q]))
+
+randomDisjunction::IO Form
+randomDisjunction = do
+  p <-randomProp
+  q <-randomProp
+  return (Dsj (p:[q]))
+
 
 exercise4 :: IO ()
 exercise4 = do
