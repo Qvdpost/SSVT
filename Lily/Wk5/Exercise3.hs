401124701f2ce845b09de204d8a50e8cfc28cfe7
Lily O'Sullivan <lilyosullivan@pm.me>
2021-10-13 00:13:24 +0200

    Arjen Code

--- Lily/Wk5/Exercise3.hs
+++ Lily/Wk5/Exercise3.hs
@@ -13,16 +13,40 @@ import Data.List
 import Helper (exercise)
 import System.Random
 import Test.QuickCheck
+import LTS
 --Time Spent:
 
-straces :: IOLTS -> [Trace]
+third :: (a, b, c) -> c
+third (_,_,s) = s
+
+step :: (Eq b, Eq a) => [(a, b, a)] -> a -> [(a, b, a)] -> [(a, b, a)]
+step tr s xs
+  | null trace = xs
+  | null xs    = step tr s trace
+  | otherwise   = step tr (third $ last xs) (xs++trace)
+  where
+      trace = filter (\(s',_,_) -> s'==s) tr
+
+{-
+  Using the step function, this function will find all reachable states,
+  but I could not get it to work so it saves the path.
+-}
+reachableStates :: LTS -> [LabeledTransition]
+reachableStates (_, _, transitions, start) = nub $ step transitions start []
+
+{-
+  This shows a list of all 'reachable' labels, but not the right strace function.
+  I tried, but could not get it to work at all.
+-}
+straces :: LTS -> [Trace]
+straces s = [map (\(_,l,_) -> l) (reachableStates s)]
 
 exercise3 :: IO ()
 exercise3 = do
-  putStrLn $ exercise 3 "XXX"
-  putStrLn $ "Example output: "
+  putStrLn $ exercise 3 "Implement: straces :: LTS -> [Trace]"
+  putStrLn "Example output: "
 
 
 _main :: IO ()
-_main = do
+_main =
   exercise3
--- Lily/Wk5/Exercise4.hs
+++ Lily/Wk5/Exercise4.hs
@@ -12,13 +12,34 @@ import Data.List
 import Helper (exercise)
 import System.Random
 import Test.QuickCheck
+import LTS
 --Time Spent:
+
+{-
+after :: LTS -> [Label] -> [State]
+(states, labels, labeledTransitions, startState) `after` transition = makeTransition
+-}
+
+third :: (a, b, c) -> c
+third (_,_,s) = s
+
+makeTransition :: State -> [LabeledTransition] -> Label -> [State]
+makeTransition s transitions l = map third $ filter (\(s',l',_) -> s == s' && l == l') transitions
+
+-- coffeeImpl1 `after` ["?coin"] -> [2]
+-- This works, but it needs to be a trace, not a single label.
+after :: LTS -> Label -> [State]
+after (_,_,transitions,start) label =
+    map third $ filter (\(s,l,_) -> s == start && l == label) transitions
+
+-- after :: LTS -> Trace -> [State]
+-- after (_,_,t,s) tr = error "not yet implemented"
 
 exercise4 :: IO ()
 exercise4 = do
   putStrLn $ exercise 4 "XXX"
-  putStrLn $ "Example output: "
+  putStrLn "Example output: "
 
 _main :: IO ()
-_main = do
+_main =
   exercise4
